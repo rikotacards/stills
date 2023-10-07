5797164be45953badcc788c41fb0f5739fc8c8a6
rikotacards <maxhsu1@gmail.com>
2023-10-07 17:22:39 +0800

    made the caption better.

--- src/components/PostHeader/PostHeader.tsx
+++ src/components/PostHeader/PostHeader.tsx
@@ -1,15 +1,17 @@
 import { MoreHoriz } from '@mui/icons-material';
-import { Avatar, Box, Typography } from '@mui/material';
+import { Avatar, Box, IconButton, Typography } from '@mui/material';
 import React from 'react';
 
 export const PostHeader: React.FC = () => {
   return (
-    <Box zIndex={1} position={'absolute'} top='0' p={2} display={'flex'} flexDirection={'row'} width={'100%'} alignItems={'center'}>
+    <Box position='relative' p={2} display={'flex'} flexDirection={'row'} width={'100%'} alignItems={'center'} >
       <Box display={'flex'} flexDirection={'row'} alignItems='center'>
         <Avatar sx={{ mr: 1 }} /><Typography>Max</Typography>
       </Box>
       <Box ml={'auto'}>
+        <IconButton>
         <MoreHoriz />
+        </IconButton>
       </Box>
     </Box>
   )
